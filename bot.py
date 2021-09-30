2187f413d285e066310461863afd1a098b0f4953
C c <53864741+chundongg@users.noreply.github.com>
2021-09-30 23:43:12 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+from graia.broadcast import Broadcast
+from graia.application import GraiaMiraiApplication, Session
+from graia.application.message.chain import MessageChain
+import asyncio
+
+from graia.application.message.elements.internal import Plain
+from graia.application.friend import Friend
+
+loop = asyncio.get_event_loop()
+
+bcc = Broadcast(loop=loop)
+app = GraiaMiraiApplication(
+    broadcast=bcc,
+    connect_info=Session(
+        host="http://localhost:8080", # 填入 httpapi 服务运行的地址
+        authKey="nwdiowamllwn", # 填入 authKey
+        account=2657661565, # 你的机器人的 qq 号
+        websocket=True # Graia 已经可以根据所配置的消息接收的方式来保证消息接收部分的正常运作.
+    )
+)
+
+@bcc.receiver("FriendMessage")
+async def friend_message_listener(app: GraiaMiraiApplication, friend: Friend):
+    await app.sendFriendMessage(friend, MessageChain.create([
+        Plain("你好，我是小鼕！我于今天出生了！")
+    ]))
+
+app.launch_blocking()
