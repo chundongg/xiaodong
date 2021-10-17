1f9af672e91bbaf0c91e99cfeaeee817e501ecb5
C c <53864741+chundongg@users.noreply.github.com>
2021-10-17 11:07:11 +0800

    Update modules_kebiao.py

--- modules/modules_kebiao.py
+++ modules/modules_kebiao.py
@@ -1,12 +1,36 @@
+from graia import application
+from graia.application import group
+from graia.application.event import dispatcher
+from graia.application.message.parser.kanata import Kanata
+from graia.application.message.parser.signature import FullMatch
 from graia.saya import Saya, Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 from graia.saya.event import SayaModuleInstalled
+from graia.saya.builtins.broadcast import ListenerSchema
+from graia.application import GraiaMiraiApplication
+from graia.application.message.chain import MessageChain
+from graia.application.message.elements.internal import Image
+from graia.application.group import Group
+from graia.application.event.messages import GroupMessage
 
 saya = Saya.current()
 channel = Channel.current()
+app: GraiaMiraiApplication
+group: Group
 
 @channel.use(ListenerSchema(
     listening_events=[SayaModuleInstalled]
 ))
 async def module_listener(event: SayaModuleInstalled):
-    print(f"{event.module}::模块加载成功!!!")
+    print(f"{event.module}::课表模块加载成功!!!")
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage]
+    )
+)
+async def module_kebiao_group(event: GroupMessage):
+    print("课表模块被触发!")
+    await app.sendGroupMessage(group,MessageChain.create([
+        Image.fromLocalFile("./images/kebiao/kebiao.jpg")    
+    ]))
