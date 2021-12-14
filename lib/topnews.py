56e42e3a874849705e4363710b1feb73508bfb78
C c <53864741+chundongg@users.noreply.github.com>
2021-12-14 12:14:59 +0800

    Update topnews.py

--- lib/topnews.py
+++ lib/topnews.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 def topnews_dayli(keyword="top"):
-    r = requests.get("http://v.juhe.cn/toutiao/index?type={}&key=1588f762e5527ba9c4c3545a0319dc81".format(keyword))
+    r = requests.get("api".format(keyword))
     jsontopython = json.loads(r.text)
     word_out = "今日新闻:"
     for i in range(5):
