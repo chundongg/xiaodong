529d32bdd003678c3a8a2fa5d7446ef0ab7abbaa
C c <53864741+chundongg@users.noreply.github.com>
2021-12-14 12:14:47 +0800

    Update gushi.py

--- lib/gushi.py
+++ lib/gushi.py
@@ -1,5 +1,5 @@
 import requests
 
 def get_gushi():
-    r = requests.get("http://api.tianapi.com/verse/index?key=f9780fbde529acd4c49c317d72073039")
+    r = requests.get("api")
     return r.text[r.text.find('content')+10:r.text.find('source')-3]+'\n'+'——《'+r.text[r.text.find('source')+9:r.text.find('author')-3]+'》'+r.text[r.text.find('author')+9:-4]
