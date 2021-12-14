1ebcf605a860ced980f74a42e9fcd6da11fe4704
C c <53864741+chundongg@users.noreply.github.com>
2021-12-14 12:14:57 +0800

    Update sudoku.py

--- lib/sudoku.py
+++ lib/sudoku.py
@@ -3,15 +3,15 @@ import json
 from PIL import Image, ImageDraw, ImageFont
 
 def sudoku(difficulty):
-    r = requests.get("http://apis.juhe.cn/fapig/sudoku/generate?key=142a30dc17d224150da21835284e9b49&difficulty={}".format(difficulty))
+    r = requests.get("api".format(difficulty))
     jsontopython = json.loads(r.text)
-    shudu = Image.open("D:/Users/Administrator/Desktop/shudu.PNG")
+    shudu = Image.open("本地文件夹") #本地文件夹
     draw = ImageDraw.Draw(shudu)
-    fnt = ImageFont.truetype(r'C:\Windows\Fonts\simkai.ttf',36)
+    fnt = ImageFont.truetype(r'C:\Windows\Fonts\simkai.ttf',36) #本地字体
     for i in range(0,9):
         for y in range(0,9):
             if (((jsontopython["result"])["puzzle"])[i])[y] != 0:
                 draw.text((((shudu.size[0]/9)*(i)+9),(shudu.size[1]/9)*(y)),str((((jsontopython["result"])["puzzle"])[i])[y]),fill='black',font=fnt)
             else:
                 continue
-    shudu.save("D:/Users/Administrator/Desktop/shudu1.PNG")
+    shudu.save("本地文件夹") #本地文件夹
